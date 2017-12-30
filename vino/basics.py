af8f29a12dee80cb78dae54941dbc82be69fea1d
mike <verysimple@gmail.com>
2017-12-30 08:22:17 +0800

    more logic for handling array items
    organize the declaration of a group of processors to apply to a group of
    items in an array.

--- vino/basics.py
+++ vino/basics.py
@@ -57,6 +57,11 @@ class VinoSchema:
             processors = [name]+processors
         self.make_batch(*processors)
 
+    def qualify(self, qualifier, *a, **kw):
+        """ just a generic adapter using the visitor pattern """
+        qualifier.qualify(self, *a, **kw)
+        
+
 
 class ObjectSchema(VinoSchema): pass
 class ObjectPropertySchema(VinoSchema): pass
@@ -81,14 +86,70 @@ class BasicSchema(VinoSchema):
         if errors: 
             raise errors.ErrorCollection(errors)
 
+PER_ITEM = 0
+PER_PROCESSOR = 1
 
-class ArraySchema(VinoSchema):
+class ArraySchema(VinoSchema, Qualifier):
     def make_batch(self, *processors):
         for p in processors:
             if p 
 
+    def validate(self, data, ordering_scheme=PER_ITEM):
+        pass
+
+    def qualify(self, item, index):
+        pass
+
 class ArrayItemSchema(VinoSchema):
     def make_batch(*processors):
         for p in processors:
-            if isinstance(p,):
+            if isinstance(p, ProcessorBundle):
 
+class Qualifier:
+    def qualify(self, item, index=None):
+        if item
+
+def sequence(self, start=0, stop=0, step=1):
+    for n,s in dict(start=start, stop=stop, step=step):
+        try:
+            s = abs(s)-1 if n=='step' else s
+            if s < 0 or not isinstance(s, int):
+                raise TypeError()
+        except TypeError:
+            raise VinoError(
+                'Invalid {} value given for sequence: {}'.format(n,s))
+    self._seq = (start, stop, step)
+seq = sequence
+
+
+
+class ProcessorBundle:
+    """ This class packs a sequence of processors to apply to a group
+    of items. """
+    def __init__(self, *processors):
+    
+    def apply_to(*qualifiers):
+        """a qualifier can be many things:
+            - a range in the form of a range object or a list: in which case
+            the listed validation steps will apply to all items whose index
+            falls within the list returned by the range. 
+            - a function: which should return True for an item before the 
+            validation steps proceed.
+        """
+
+
+    def _init_qualifier(self, qualifier):
+        pass
+
+    def _valid_range(self, r):
+        if r is None or 0:
+            return list(range(0))
+        if isinstance(r, range):
+            return list(r)
+
+    def _empty_list(self, l):
+        return l==[]
+
+        
+some = ProcessorBundle
+every = lambda *a: return ProcessorBundle(0, *processors)
